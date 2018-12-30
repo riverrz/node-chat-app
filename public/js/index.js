bb90073eb8fa976c17c1fe1da0d71e20144df51d
shivam123425 <shivam123425@gmail.com>
2018-12-30 21:49:17 +0530

    Improve UX

--- public/js/index.js
+++ public/js/index.js
@@ -17,15 +17,18 @@ socket.on("newLocationMessage", function(message) {
   $("#messages").append(li);
 });
 
+var messageTextbox = $("[name=message]");
 $("#message-form").on("submit", function(e) {
   e.preventDefault();
   socket.emit(
     "createMessage",
     {
       from: "User",
-      text: $("[name=message]").val()
+      text: messageTextbox.val()
     },
-    function() {}
+    function() {
+      messageTextbox.val("");
+    }
   );
 });
 
@@ -34,14 +37,17 @@ locationButton.on("click", function() {
   if (!navigator.geolocation) {
     return alert("Geolocation not supported by your browser");
   }
+  locationButton.attr("disabled", "disabled").text("Sending location...");
   navigator.geolocation.getCurrentPosition(
     function(position) {
+      locationButton.removeAttr("disabled").text("Send location");
       socket.emit("createLocationMessage", {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     },
     function() {
+      locationButton.removeAttr("disabled").text("Send location");
       alert("Unable to fetch location");
     }
   );
